d27d52ae31b5d2fe4e60026d089638b7e9fce68f
Adam Cook <43277610+ajcook247@users.noreply.github.com>
2024-02-06 12:36:41 -0500

    Pushing graph changes

--- makeBetterGraph.py
+++ makeBetterGraph.py
@@ -10,23 +10,25 @@ import os
 import numpy as np
 import yaml
 
-def makemAPGraph(path, augName, modelName):
+def makemAPGraph(path, augName, modelName, isCompound): #isCompound:bool
     path = "./" + path
     map50s = np.load( os.path.join(path, 'graphing.npy'), allow_pickle=True )
     map50s = map50s.item()
     with open( os.path.join(path, 'meta.yaml') , 'r') as f:
         metadata = yaml.safe_load(f)
-    #print(metadata.keys())
     plt.figure(figsize=(7.5,5))
-    print(map50s)
-    print()
-    print(metadata)
-    print("=================")
 
-    for i, noise in enumerate(metadata.keys()):
-        xaxis = metadata[noise]
-        mAP = map50s[noise][0]
-        plt.plot(xaxis, mAP,'-o')
+    # Compounding only - xaxis determined by fixed number set (change later - needs to scale)
+    if isCompound:
+        xaxis = [1, 2, 3, 4, 5]
+        mAP = map50s['Compound'][0]
+        plt.plot(xaxis, mAP, '-o')
+    # Standard graph preparation
+    else:
+        for i, noise in enumerate(metadata.keys()): # Use isCompound to ignore this process
+            xaxis = metadata[noise]
+            mAP = map50s[noise][0]
+            plt.plot(xaxis, mAP,'-o')
 
     plt.title(augName + ' vs ' + modelName + ' mAP')
     plt.ylabel('mAP')
